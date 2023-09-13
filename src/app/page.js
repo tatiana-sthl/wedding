bf40b7e034d0c8e6e1f8c26f010479c227c4c3f6
unknown <stahli.tatiana@gmail.com>
2023-09-13 17:38:45 +0200

    add date

--- src/app/page.js
+++ src/app/page.js
@@ -11,25 +11,20 @@ export default function Home() {
     <main className={styles.main}>
       <nav className={styles.nav}>
         <ul className={styles.ul}>
-          <li><a href="#">Tatiana & Baptiste</a></li>
+          <li className='home'><a href="#">Tatiana & Baptiste</a></li>
           <li className='center'><a href="#">Programme</a></li>
           <li className='upward'><a href="#">Infos pratiques</a></li>
           <li className='forward'><a href="#">RSVP</a></li>
         </ul>
       </nav>
       
-      <div>
-        <div>
-          <h1>Samedi 7 septembre 2024</h1>
-          <p>
-            Enfin ! Après 10 ans de vie commune (12 quand nous serons à la date du mariage), nous allons nous marier et nous n’avons qu’une hâte, pouvoir le fêter avec vous !
-            Pour cela, vous trouverez sur ce site toutes les informations nécessaire, et surtout le formulaire de confirmation de votre présence.
-            Garder précieusement le lien de ce site, car c’est ici que vous pourrez retrouver les photos prises lors du week-end, et vous pourrez même partager les vôtres.
-            Et en attendant le grand jour, un simple merci de faire partie de nos vies.
-          </p>
-          <h2>Décompte</h2>
-          <p id='countdown'></p>
-        </div>
+      <div className={styles.textePresentation}>
+        <h1 className={styles.h1}>Samedi 7 septembre 2024</h1>
+        <p className={styles.p}>
+          Enfin ! Après 10 ans de vie commune (12 quand nous serons à la date du mariage), nous allons nous marier et nous n’avons qu’une hâte, pouvoir le fêter avec vous !
+          Pour cela, vous trouverez sur ce site toutes les informations nécessaire, et surtout le formulaire de confirmation de votre présence.
+          Et en attendant le grand jour, un simple merci de faire partie de nos vies.
+        </p>
 
       </div>
     </main>
