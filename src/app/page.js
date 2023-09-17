be6862c2c26c468b2aa3bfb93ea1d198d08e0441
unknown <stahli.tatiana@gmail.com>
2023-09-17 09:50:08 +0200

    timeline ok, try to add animation

--- src/app/page.js
+++ src/app/page.js
@@ -26,14 +26,40 @@ export default function Home() {
           Et en attendant le grand jour, un simple merci de faire partie de nos vies.
         </p>
       </div>
-      <div>
+      <div className={styles.presentation}>
         <h2 className={styles.h2}>Tout commence par...</h2>
-        <div className={styles.planning}>
-          <div className={styles.orga}>
-
-          </div>
-          <div className={styles.orga}>
-
+        <div className={styles.timeline}>
+          <div className={styles.outer}>
+            <div className={styles.card}>
+              <div className={styles.info}>
+                <h3 className={styles.title}>Title 1</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.info}>
+                <h3 className={styles.title}>Title 2</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.info}>
+                <h3 className={styles.title}>Title 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.info}>
+                <h3 className={styles.title}>Title 4</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.info}>
+                <h3 className={styles.title}>Title 5</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
