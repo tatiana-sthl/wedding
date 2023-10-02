d8e08e228e2035888407b84867a5da5c01803076
unknown <stahli.tatiana@gmail.com>
2023-10-02 19:13:43 +0200

    try another method for scroll

--- src/app/components/timeline.js
+++ src/app/components/timeline.js
@@ -1,45 +1,55 @@
-import React, { useEffect, useState } from 'react';
 import styles from '../page.module.css';
 
 
 const Timeline = () => {
 
-  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+
   return (
-    <div className={styles.timeline}>
+    <section className={styles.timeline}>
     <div className={styles.outer}>
-      <div className={styles.card}>
+      <div className={`${styles.card} hidden`}>
         <div className={styles.info}>
           <h3 className={styles.title}>Rdv à l'Eglise</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
       </div>
-      <div className={styles.card}>
+      <div className={`${styles.card} hidden`}>
         <div className={styles.info}>
           <h3 className={styles.title}>Title 2</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
       </div>
-      <div className={styles.card}>
+      <div className={`${styles.card} hidden`}>
         <div className={styles.info}>
           <h3 className={styles.title}>Title 3</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
       </div>
-      <div className={styles.card}>
+      <div className={`${styles.card} hidden`}>
         <div className={styles.info}>
           <h3 className={styles.title}>Title 4</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
       </div>
-      <div className={styles.card}>
+      <div className={`${styles.card} hidden`}>
         <div className={styles.info}>
           <h3 className={styles.title}>Title 5</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
       </div>
     </div>
-  </div>
+  </section>
   );
 };
 
