bda85b3ee6e814ee50faa5f6960bd2e4a62ffbc2
unknown <stahli.tatiana@gmail.com>
2023-10-17 10:54:58 +0200

    confirm about email send

--- src/app/components/rsvp.js
+++ src/app/components/rsvp.js
@@ -8,6 +8,7 @@ const Rsvp = () => {
     email: '',
     message: '',
   });
+  const [isEmailSent, setIsEmailSent] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,8 +29,7 @@ const Rsvp = () => {
     });
 
     if (response.status === 200) {
-      // Gérer le succès de l'envoi
-      console.log('E-mail envoyé avec succès.');
+      setIsEmailSent(true);
     } else {
       // Gérer l'échec de l'envoi
       console.error('Erreur lors de l\'envoi de l\'e-mail.');
@@ -37,59 +37,65 @@ const Rsvp = () => {
   };
   
   return (
-    <form className={styles.contactForm} onSubmit={handleSubmit}>
-      <div className={styles.mailInfos}>
-        <div className={styles.formGroup}>
-          <label className={styles.label} htmlFor="email">Email :</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className={styles.input}
-            placeholder='exemple@gmail.com'
-          />
-        </div>
+    <div>  
+      {isEmailSent && <p className={styles.success}>Email envoyé avec succès!</p>}
+      <form className={styles.contactForm} onSubmit={handleSubmit}>
+        <div className={styles.mailInfos}>
+          <div className={styles.formGroup}>
+            <label className={styles.label} htmlFor="email">Email :</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className={styles.input}
+              placeholder='exemple@gmail.com'
+            />
+          </div>
 
-        <div className={styles.formGroup}>
-          <label className={styles.label} htmlFor="name">Nom :</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
+          <div className={styles.formGroup}>
+            <label className={styles.label} htmlFor="name">Nom :</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className={styles.input}
+              placeholder='Petit Prince'
+            />
+          </div>
+        </div>
+              
+        <div className={styles.formGroupTextarea}>
+          <label className={styles.label} htmlFor="message">Message :</label>
+          <ul>
+            Merci de préciser dans le mail : 
+            <li>le nombre de personnes qui seront présentes ainsi que leur nom complet. </li>
+            <li>le nombre d'enfants ainsi que leur âge.</li>
+          </ul>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
             onChange={handleChange}
-            className={styles.input}
-            placeholder='Petit Prince'
+            className={styles.textarea}
+            placeholder=
+            {`Bonjour vous deux,
+            Nous serons trois pour le vin d'honneur/repas :
+            - L'empereur
+            - Sa femme
+            - Le petit prince (5 ans)
+            Au plaisir d'y être !`}
           />
         </div>
-      </div>
-            
-      <div className={styles.formGroupTextarea}>
-        <label className={styles.label} htmlFor="message">Message :</label>
-        <ul>
-          Merci de préciser dans le mail : 
-          <li>le nombre de personnes qui seront présentes ainsi que leur nom complet. </li>
-          <li>le nombre d'enfants ainsi que leur âge.</li>
-        </ul>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          className={styles.textarea}
-          placeholder=
-          {`Bonjour vous deux,
-          Nous serons trois pour le vin d'honneur/repas :
-          - L'empereur
-          - Sa femme
-          - Le petit prince (5 ans)
-          Au plaisir d'y être !`}
-        />
-      </div>
-      <button className={styles.button} type="submit">Envoyer</button>
-    </form>
+        <div className={styles.formButton}>
+          <button className={styles.button} type="submit">Envoyer</button>
+        </div>
+        
+      </form>
+    </div>
   );
 };
 
