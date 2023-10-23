eeab42708884504a20033c64333fd78138c677ff
unknown <stahli.tatiana@gmail.com>
2023-10-23 19:29:41 +0200

    adapt hamburger menu like evekinesio

--- src/app/components/menu.js
+++ src/app/components/menu.js
@@ -9,12 +9,27 @@ function Menu() {
   };
 
   return (
-    <div>
-      <button onClick={toggleMenu} className={styles.hamburgerButton}>
-        &#9776; {/* Cela affiche le symbole hamburger (trois lignes) */}
-      </button>
+    <>
+      <div className={styles.navbar}>
+        <div className={`${styles.containermenu} ${styles.navcontainermenu}`}>
+            <input className={styles.checkbox} type="checkbox" name="" id="" />
+            <div className={styles.hamburgerlines}>
+              <span className={`${styles.line} ${styles.line1}`}></span>
+              <span className={`${styles.line} ${styles.line2}`}></span>
+              <span className={`${styles.line} ${styles.line3}`}></span>
+            </div>  
+          <div className={styles.menuitems}>
+            <li><a href="#">Presentation</a></li>
+            <li><a href="#">Mes pratiques</a></li>
+            <li><a href="#">Avis</a></li>
+            <li><a href="#">Planning</a></li>
+            <li><a href="#">Me contacter</a></li>
+          </div>
+        </div>
+      </div>
 
-      <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
+
+      <nav className={styles.nav}>
         <ul className={styles.ul}>
           <li className='home'><a href="#">Tatiana & Baptiste</a></li>
           <li className='center'><a href="#timeline">Programme</a></li>
@@ -22,7 +37,10 @@ function Menu() {
           <li className='forward'><a href="#form">RSVP</a></li>
         </ul>
       </nav>
-    </div>
+    </>
+
+      
+
   );
 }
 
