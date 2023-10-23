c98caef0f494433a704632c7308a44e44dff1eed
unknown <stahli.tatiana@gmail.com>
2023-10-23 19:13:14 +0200

    try first solution

--- src/app/components/menu.js
+++ src/app/components/menu.js
@@ -1,9 +1,20 @@
 import styles from '../../app/page.module.css';
+import React, { useState } from 'react';
 
 function Menu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <nav className={styles.nav}>
+    <div>
+      <button onClick={toggleMenu} className={styles.hamburgerButton}>
+        &#9776; {/* Cela affiche le symbole hamburger (trois lignes) */}
+      </button>
+
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
         <ul className={styles.ul}>
           <li className='home'><a href="#">Tatiana & Baptiste</a></li>
           <li className='center'><a href="#timeline">Programme</a></li>
@@ -11,7 +22,9 @@ function Menu() {
           <li className='forward'><a href="#form">RSVP</a></li>
         </ul>
       </nav>
+    </div>
   );
 }
 
 export default Menu;
+
